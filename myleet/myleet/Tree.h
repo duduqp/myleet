b7d5131e62bd747018167f63028d49209e69c723
杜清鹏 <1361229831@qq.com>
2020-01-05 00:09:59 +0800

    reconstruct avl

--- myleet/myleet/Tree.h
+++ myleet/myleet/Tree.h
@@ -1,159 +1,187 @@
 #pragma once
-#include <stdexcept>
-#include <exception>
-#include <algorithm>
-#include <memory>
-#include <stack>
-#include <utility>
 #include <iostream>
+#include <memory>
+#include <algorithm>
 using namespace std;
-/*
-implement a binary search tree
-*/
-enum OD {
-	PRE,IN,POST,LEVEL
-};
-class Tree
+class tree
 {
 public:
-	struct TreeNode {
-		struct TreeNode * l;
-		struct TreeNode * r;
-		int d;
-		int h;
-		TreeNode(struct TreeNode * _l, struct TreeNode * _r, int _d
-			, int _h) :l(_l), r(_r), d(_d), h(_h) { }
-		
+	struct node {
+		struct node *l;
+		struct node *r;
+		struct node *f;
+		int d, h;
+		node(struct node *_l, struct node *_r,int _d,int _h,struct node * _f):l(_l),r(_r),d(_d),h(_d),f(_f){ }
 	};
-	Tree();
 
-	const int height() const {
-		if (!root)
-		{
-			//empty
-			return 0;
-		}
-		else {
-			return root->h;
-		}
+	static void rotate_LL(node * root)
+	{
+
 	}
+
+	tree(){}
+	tree(const tree & rhs)
+	{
+
+	}
+	~tree()
+	{
+
+	}
+
+	tree & operator=(const tree & rhs)
+	{
+
+	}
+	const int height() const
+	{
+		return root->h;
+	}
+
+	const bool empty() const {
+		return _size == 0;
+	}
+
 	const int size() const {
 		return _size;
-	}
-	const void display(OD od) const {
-		//based on traverse procedure
-		switch (od) {
-		case OD::PRE:
 
-			break;
-		case OD::IN:
-			break;
-		case OD::POST:
-			break;
-
-		default :
-
-		}
 	}
 
-	TreeNode * search(int d){
-		TreeNode * tmp=root;
-		stack<shared_ptr<Tree::TreeNode> > s;
-		return search_at(tmp, d,s);
+	bool contains(int d) {
+		return search(d) == nullptr;
 	}
+
 	void insert(int d)
 	{
-		TreeNode * tmp = root;
-		if (!insert_at(tmp, d)) { 
-			throw invalid_argument("no such elem");
-		}
+
 	}
 
 	void remove(int d)
 	{
-		TreeNode * tmp = root;
-		if (!remove_at(tmp, d))
-		{
-			throw invalid_argument("no such elem");
-		}
+
 	}
 
-	virtual ~Tree();
-private:
-	TreeNode * insert_at(TreeNode * root, int d)
+	node * search(int d)
 	{
-		TreeNode * tmp = root;
-		TreeNode * ret = root;
-		stack<shared_ptr<Tree::TreeNode> > s;
-		if ((ret=search_at(tmp,d,s)))
+
+	}
+
+private:
+	node * root;
+	int _size;
+
+	//auxiliary
+	node * search_at(node * root, int d)
+	{
+		if (!root)
 		{
-			//already exists
-			return ret;
+			return root;
 		}
-		//not exists ,note the tmp cursor has moved to the proper node to insert,and stack cache the path
-		cout << s.size() << endl;
-		if (tmp->d < d)
+		if (root->d == d)
 		{
-			//insert at right
-			tmp->r = new TreeNode(nullptr, nullptr, d, 1);
-			return tmp->r;
+			return root;
+		}
+		else if (root->d < d)
+		{
+			return search_at(root->r,d);
 		}
 		else {
-			tmp->l = new TreeNode(nullptr, nullptr, d, 1);
-			return tmp->l;
+			return search_at(root->l, d);
 		}
 	}
 
-	TreeNode * remove_at(TreeNode * r, int d)
+	node * insert_at(node * &root, int d)
 	{
-
-
-
-	}
-
-	TreeNode * search_at(TreeNode *& root, int d,stack<shared_ptr<Tree::TreeNode> > &s)
-	{
-		while (root)
+		if (!root)
 		{
-			auto p = make_shared<Tree::TreeNode>(*root);
-			s.push(p);
-			if (root->d == d)
+			root = new node(nullptr, nullptr, d, 1,root);
+		}
+		else if (root->d == d)
+		{
+			;
+		}
+		else if (root->d < d)
+		{
+			root->r = insert_at(root->r, d);
+		}
+		else {
+			root->l = insert_at(root->l, d);
+		}
+		return root;
+	}
+	
+	node * remove_at(node * root, int d)
+	{
+		node * tmp = search_at(root,d);
+		if (!tmp)
+		{
+			return root;
+		}
+		node * f = tmp->f;
+		if ((!tmp->l) && ((!tmp->r)))
+		{
+			//leaf
+			if (tmp == tmp->f->l)
 			{
-				return root;
-			}
-			else if (root->d > d)
-			{
-				if (root->l)
-				{
-					root = root->l;
-				}
-				else {
-					//no left child
-					return nullptr;
-				}
+				//be a left child
+				f->l = nullptr;
 			}
 			else {
-				if (root->r)
-				{
-					root = root->r;
-				}
-				else {
-					return nullptr;
-				}
+				//be a right child
+				f->r = nullptr;
 			}
+
+			delete tmp;
+		}
+		else if (tmp->l&&(!tmp->r))
+		{
+			//has left child but not a right child
+			if (tmp == f->l)
+			{
+				f->l = tmp->l;
+			}
+			else {
+				f->r = tmp->l;
+			}
+
+			delete tmp;
+		}
+		else if (tmp->r && (!tmp->l))
+		{
+			//has left child but not a right child
+			if (tmp == f->l)
+			{
+				f->l = tmp->r;
+			}
+			else {
+				f->r = tmp->r;
+			}
+
+			delete tmp;
+		}
+		else {
+			//two children
+			//find precedentor on right subtree
+			node * precedentor = tmp->r;
+			while (precedentor->l)
+			{
+				precedentor = precedentor->l;
+			}
+
+			tmp->d = precedentor->d;
+			remove_at(tmp->r, precedentor->d);
 		}
 	}
-	int height(TreeNode * root)
+
+	int height_update(node * root)
 	{
 		if (!root)
 		{
 			return 0;
 		}
-		root->h = max(height(root->l), height(root->r)) + 1;
-		return root->h;		
+		else {
+			root->h = max(height_update(root->l), height_update(root->r)) + 1;
+			return root->h;
+		}
 	}
-	
-	TreeNode * root;
-	int _size;
 };
-
