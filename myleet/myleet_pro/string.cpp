240bac1d6dc6e50da1bb1af51209f1cc84540158
杜清鹏 <1361229831@qq.com>
2020-02-15 17:14:07 +0800

    new solu

---
+++ myleet/myleet_pro/string.cpp
@@ -0,0 +1,36 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+using namespace std;
+//pattern match
+void next_kmp(const string & p,vector<int> & next)
+{
+
+
+}
+
+int kmp(const string & p,const string & t)
+{
+
+}
+
+void next_bm()
+{
+
+}
+
+int bm()
+{
+
+}
+
+
+
+int main()
+{
+
+
+    std::cout << "Hello World!\n"; 
+}
